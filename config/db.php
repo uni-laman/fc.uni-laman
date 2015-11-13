11d7177b37ac0ec7179e98c25e5eb4bf6b05030d
Vit Abrazumov <vitabrazumov@hotmail.com>
2015-11-13 14:40:19 +0200

    db

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=unilaman.mysql.ukraine.com.ua;dbname=unilaman_fc',
+    'username' => 'unilaman_fc',
+    'password' => '3g9n8v2w',
     'charset' => 'utf8',
 ];
