e3cea0c42a2a568e84357796f5337429700b9d1d
Vit Abrazumov <vitabrazumov@hotmail.com>
2015-11-13 16:04:33 +0200

    index

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,38 +27,89 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => '<img src="/web/img/top_logo2.png" class="leftimg">',
         'brandUrl' => Yii::$app->homeUrl,
+        'brandOptions'=>['class'=>'navbar-brand'],
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar',
+            'id'=>'custom-bootstrap-menu',
+        ],
+        'renderInnerContainer'=>true,
+        'innerContainerOptions'=>[
+            'class'=>'container',
         ],
     ]);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            
             Yii::$app->user->isGuest ?
-                ['label' => 'Login', 'url' => ['/site/login']] :
+                ['label' => 'Личный кабинет', 'url' => ['/site/login']] :
                 [
                     'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
                     'url' => ['/site/logout'],
                     'linkOptions' => ['data-method' => 'post']
                 ],
+            ['label' => 'Регистрация', 'url' => ['/site/about']],
+            ['label' => 'Ссылки', 'url' => ['/site/contact']],
         ],
     ]);
+    echo "<form class='navbar-form navbar-right' role='search'>
+       <div class='form-group has-feedback'>
+            <input id='searchbox' type='text' placeholder='Поиск' class='form-control'>
+            <span id='searchicon' class='fa fa-search form-control-feedback'></span>
+        </div>
+  </form>";
+    echo "<br><div  id='answer' class='bg-success' style='position: fixed;
+                      top: 50px; 
+                     
+                      background: #627ba3;
+                      
+                      text-color: #ccc;
+                      //width: 100%;
+                      height: 46px;
+                      text-align: center;
+                      z-index: 1;
+                     
+
+
+
+
+
+                      '>
+                <ul  class='list-inline' style='color:white; padding-top:12px;'>
+                <li ><a class='menuahref' href='/web'>&nbsp</a></li>
+                <li ><a class='menuahref' href='/web'>&nbsp;</a></li>
+                            <li ><a class='menuahref' href='/web'>&nbsp;</a></li>
+                            <li ><a class='menuahref' href='/web'>&nbsp;</a></li>
+                            <li ><a class='menuahref' href='/web'>Главная</a></li>
+                            <li ><a class='menuahref' href='/web/club'>О клубе</a></li>
+                            <li ><a class='menuahref' href='/web/command'>Команда</a></li>
+                            <li ><a class='menuahref' href='/web/news'>Новости</a></li>
+                            <li ><a  class='menuahref' href='/web/games'>Игры</a></li>
+                            <li ><a class='menuahref' href='/web/multi'>Мультимедия</a></li>
+                            <li ><a class='menuahref' href='/web/fanzone'>Фан зона</a></li>
+                            <li ><a class='menuahref' href='/web/partners'>Партнеры</a></li>
+                            <li ><a class='menuahref' href='/web/contact'>Контакты</a></li>
+                            <li ><a class='menuahref' href='/web'>&nbsp;</a></li>
+                            <li ><a class='menuahref' href='/web'>&nbsp;</a></li>
+                          
+
+                        </ul>
+                </div>";
+    
     NavBar::end();
     ?>
+   
 
-    <div class="container">
+    <div class="container body-background">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= $content ?>
     </div>
 </div>
-
+<!--
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
@@ -66,7 +117,48 @@ AppAsset::register($this);
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
-
+-->
+    <div id="footer" style="background:-webkit-linear-gradient(top, #627ba3 2%,#93A3BD 100%);">
+<div class="container">
+  <div class="row">
+    
+    <div id="bottom-footer">
+      <div class="row">
+        <div class="col-md-8">
+            
+        </div>
+        <div class="col-md-4 " style="margin-top: 10px;" >
+            <img class="socialgrupps" src="/web/img/vk.png"  alt="vkontakte">
+            <img class="socialgrupps" src="/web/img/facebook.png"  alt="facebook">
+            
+            <img style="align:right;" class="socialgrupps" src="/web/img/google.png"  alt="google">
+          </div>
+    </div>
+  </div><!--/row 2-->
+      <div class="row" id="final-footer">
+          <div class="col-sm-4">  </div>
+        <div class="col-sm-4 text-center">
+        	Copyright © <?= date('Y') ?> FC.uni-laman. All Rights Reserved.<br/>
+			 Odessa - Ukraine
+        </div>
+          
+        <div class="col-sm-4 text-right">
+        	<span class="fa-stack fa-lg">
+              <i class="fa fa-circle text-primary fa-stack-2x"></i>
+              <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+            </span>
+          <span class="fa-stack fa-lg">
+              <i class="fa fa-circle text-info fa-stack-2x"></i>
+              <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+            </span>
+          <span class="fa-stack fa-lg">
+              <i class="fa fa-circle text-danger fa-stack-2x"></i>
+              <i class="fa fa-youtube fa-stack-1x fa-inverse"></i>
+            </span>
+        </div>
+      </div>
+  </div><!--/container-->
+</div><!--/footer--></div>
 <?php $this->endBody() ?>
 </body>
 </html>
