82caa35ac06973c61b6264c0a3a80aa6d2e3bbb9
Vit Abrazumov <vitabrazumov@hotmail.com>
2015-11-13 14:22:07 +0200

    miha kachai

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'My Company1',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
